3f0ed4a4ef38d3f8c99c6e5abaea182f4019c4df
Taoudi <taoudi128@gmail.com>
2019-04-30 17:20:52 +0200

    test

--- scripts/file.py
+++ scripts/file.py
@@ -1,3 +1,14 @@
-file = open("file.txt", 'w')
-file.write("cool")
-file.close()
+from DBHandler import DBHandler
+
+#DBHandler().get_all_word()
+
+
+def get_all_word(self):
+    cursor = self.conn.cursor()
+    cursor.execute("SELECT word,fear FROM words")
+    result = cursor.fetchall()
+    # print("[word - occurence]")
+    # print(result)
+    for x in sorted(result, key=lambda x: int(x[1]), reverse=True):
+        if x[1] > 1000:
+            print(x[0] + " & " + str(x[1]) + "\\" + "\\")
