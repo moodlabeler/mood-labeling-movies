387b15ddccc23134987e673a5d7e50155adcc691
Taoudi <taoudi128@gmail.com>
2019-05-20 10:29:54 +0200

    testing

--- scripts/classifier_script.py
+++ scripts/classifier_script.py
@@ -12,63 +12,60 @@ def print_results(tot_movies, correct_class, result_file):
     print("Total movies: ", tot_movies)
     print("Correct guesses: ", correct_class)
     print("Correct percentage: ", correct_class / tot_movies)
-    result_file.write("Total movies: " + str(tot_movies) + "\n")
-    result_file.write("Correct guesses: " + str(correct_class) + "\n")
-    result_file.write("Correct percentage: " + str(correct_class / tot_movies) + "\n")
-    result_file.close()
     print("------------------------")
     print("Done")
     print("------------------------")
 
 
-def print_start():
-    file_name = str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")) + ".txt"
-    print("------------------------")
-    print("Printing results to file ", file_name)
-    print("------------------------")
-    return open(file_name, 'w')
-
-
 def calculate_results(TP, FP, FN, tot_movies, correct_guess):
-    if FP==0 and TP == 0:
+    if FP == 0 and TP == 0:
         precsision = -1
     else:
         precsision = TP / (TP + FP)
-
-    recall = TP / (TP + FN)
+    if TP == 0 and FN == 0:
+        recall = -1
+    else:
+        recall = TP / (TP + FN)
     accuracy = correct_guess / tot_movies
     print("Recall: ", recall)
     print("Precsision: ", precsision)
-    print("Accuracy: ", accuracy)
+    # print("Accuracy: ", accuracy)
+    print("TP : " + str(TP) + ", FP : " + str(FP) + " , FN : " + str(FN))
     print("***********************")
 
 
 movies = DBHandler().get_test_movies()
-result_file = print_start()
+results = []
 moods = ["sadness", "fear", "joy", "surprise"]
+tot_movies = 0
+for movie in movies:
+    tot_movies += 1
+    id = movie[0]
+    actual__mood = movie[1]
+    # title = movie[3]
+    b = BayesClassifier()
+    predicted_mood = b.label(id)
+    print(actual__mood + " - " + predicted_mood)
+    results.append([actual__mood, predicted_mood])
+
+
 for current_mood in moods:
+    print("Calculating for: " + current_mood)
     correct_guess = 0
-    tot_movies = 0
     TP = 0
     FP = 0
     FN = 0
-    print("Evaluating ", current_mood)
-    for movie in movies:
-        tot_movies += 1
-        id = movie[0]
-        actual__mood = movie[1]
-        # title = movie[3]
-        b = BayesClassifier()
-        predicted_mood = b.label(id)
-        #print(actual__mood + " - " + predicted_mood)
+    for result in results:
+        predicted_mood = result[1]
+        actual__mood = result[0]
         if current_mood == predicted_mood and actual__mood == predicted_mood:
-            TP += 1
+                TP += 1
         elif current_mood == predicted_mood and actual__mood != predicted_mood:
-            FP += 1
-        else:
-            FN += 1
+                FP += 1
+        elif current_mood == actual__mood and actual__mood != predicted_mood:
+                FN += 1
         if predicted_mood == actual__mood:
-            correct_guess+=1
+                correct_guess += 1
     calculate_results(TP, FP, FN, tot_movies, correct_guess)
 
 # print_results(tot_movies, correct_class, result_file)
